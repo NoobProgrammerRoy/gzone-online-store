61e84826176554e28e8e587d2b5a7f884f91d903
NoobProgrammerRoy <60422573+NoobProgrammerRoy@users.noreply.github.com>
2021-03-11 18:01:03 +0530

    Fixed game page buttons

--- public/js/product-items.js
+++ public/js/product-items.js
@@ -1,6 +1,11 @@
 const addToCart = document.getElementById('addToCart')
+const removeFromCart = document.getElementById('removeFromCart')
 const gameTitle = document.getElementById('gameTitle').innerText
 
 addToCart.addEventListener('click', () => {
     alert(`${gameTitle} Added to Cart!`)
+})
+
+removeFromCart.addEventListener('click', () => {
+    alert(`${gameTitle} Removed from Cart!`)
 })
